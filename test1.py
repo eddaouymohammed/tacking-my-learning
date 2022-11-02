b50159c8b12be4b4f4d706700a4fb7817b09738a
eddaouymohammed <117194665+eddaouymohammed@users.noreply.github.com>
2022-11-02 14:39:47 -0700

    Add files via upload

---
+++ test1.py
@@ -0,0 +1,34 @@
+import random
+from unittest import result
+def get_choices ():
+    player_choice = input("Enter the player  choice (rock,paper,scissors):")
+    option = ["rock","paper","scissors"]
+    computer_choice = random.choice(option)
+    choices = {"player":player_choice , "computer":computer_choice}
+    return choices 
+
+def check_win (player ,computer):
+    print (f"you chose  {player} , computer chose  {computer}")
+    if player==computer:
+        return "its a tie!"
+    elif player == "rock":
+        if computer == "scissors":
+         return  " you  win"  
+    else :
+        return  " you lose"
+    elif player == "scissors":
+        if computer == "rock":
+         return  " you  lose"  
+    else :
+        return  " you win"
+    
+    elif player == "paper":
+        if computer == "rock":
+         return  " you  win"  
+    else :
+        return  " you lose"
+    
+
+choices = get_choices():
+result = check_win ( choices["player"] , choices ["computer"] )
+print ( result)
